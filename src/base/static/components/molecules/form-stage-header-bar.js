c33af2fc1399bc55f3442072e4c8c35084296f74
Maria Laclau <maria.laclau@grupoesfera.com.ar>
2021-12-15 02:39:04 -0300

    adds checkbox styles and dynamic colors in form header titles and icons

--- src/base/static/components/molecules/form-stage-header-bar.js
+++ src/base/static/components/molecules/form-stage-header-bar.js
@@ -1,12 +1,22 @@
 import React from "react";
 import classNames from "classnames";
 import PropTypes from "prop-types";
-
 import { Header3, Paragraph } from "../atoms/typography";
-
 import "./form-stage-header-bar.scss";
 
 const FormStageHeaderBar = props => {
+  
+  const imgPath = (qprFormType) => {switch (qprFormType) {
+    case "water":
+      return '/static/css/images/markers/marker-agua.svg';
+    case "relocations":
+      return '/static/css/images/markers/marker-relocalizaciones.svg';
+    case "protected-areas":
+      return '/static/css/images/markers/marker-areas.svg';
+    default:
+      return '/static/css/images/markers/marker-agua.svg';
+  }}
+  
   return (
     <div
       className={classNames("form-stage-header-bar", {
@@ -14,16 +24,22 @@ const FormStageHeaderBar = props => {
       })}
     >
     <div style={{display: "flex", justifyContent: "flex-start", margin: 0, padding: 0, paddingBottom: '10px'}}>
-      <Header3 classes="form-stage-header-bar__header">
+      <Header3 classes={"form-stage-header-bar__header"}>
         {props.stageConfig.icon_url && (
           <img
             className="form-stage-header-bar__icon"
             src={props.stageConfig.icon_url}
           />
         )}
-        {props.stageConfig.header}
+        <span className={classNames({
+        "text-color-protected-areas": props.stageConfig.qprFormType === "protected-areas",
+        "text-color-relocations": props.stageConfig.qprFormType === "relocations",
+        "text-color-water": props.stageConfig.qprFormType === "water"
+      })}>
+          {props.stageConfig.header}
+        </span>
       </Header3>
-      <img style={{width: '36px', marginLeft: '8px'}} src={'/static/css/images/markers/marker-agua.svg'} alt={'icono de formulario'} />
+      <img style={{width: '36px', marginLeft: '8px'}} src={imgPath(props.stageConfig.qprFormType)} alt={'icono de formulario'} />
       </div>      
   
       <Paragraph className="form-stage-header-bar__description">
